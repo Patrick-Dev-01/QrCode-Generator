e0c345a134c1a1c1bbff9dda9400bfe78f177449
Patrick <patrick.rdecarvalho@outlook.com>
2021-08-05 17:54:53 -0300

    Gerando QR Codes de outras formas: Arquivos de imagem e para URL's

--- src/config/qrcode.ts
+++ src/config/qrcode.ts
@@ -1,10 +1,24 @@
-export async function qrCodeGenerate(text: any){
+export async function qrCodeGenerate(text: any, type: string){
     const QRCode = require('qrcode');
     
     try {
-        const qrCode = await QRCode.toString(text, { type: 'terminal' })
-  
-        console.log(qrCode);
+        // Gerando o QR Code através de um arquivo
+        if(type === 'file'){
+            await QRCode.toFile(`./qrCode-image.png`, text)
+        }
+
+        // Gerando QR Code para uma URL
+        if(type === 'url'){
+            const qrCode = await QRCode.toDataURL(text)
+            console.log(qrCode)
+        }
+
+        // Gerando QR Code pelo terminal
+        else{
+            const qrCode = await QRCode.toString(text, { type: 'terminal' })
+            console.log(qrCode);
+        }
+
     } catch (err) {
         console.log(err)
     }  
--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,23 @@ const routes = Router();
 routes.post('/generate/text', (request: Request, response: Response) => {
     const { text } = request.query;
 
-    qrCodeGenerate(text)
+    qrCodeGenerate(text, 'terminal')
+
+    response.status(200).send()
+})
+
+routes.post('/generate/url', (request: Request, response: Response) => {
+    const { url } = request.query;
+
+    qrCodeGenerate(url, 'url')
+
+    response.status(200).send()
+})
+
+routes.post('/generate/file', (request: Request, response: Response) => {
+    const { text } = request.query;
+
+    qrCodeGenerate(text, 'file')
 
     response.status(200).send()
 })
